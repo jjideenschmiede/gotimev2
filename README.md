cb27b6c2e6caf9aaae53d3d57d71fca64c623aaf
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2022-01-28 15:41:25 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+gotimev2
---
+++ times.go
@@ -0,0 +1,12 @@
+//********************************************************************************************************************//
+//
+// Copyright (C) 2018 - 2022 J&J Ideenschmiede GmbH <info@jj-ideenschmiede.de>
+//
+// This file is part of gotimev2.
+// All code may be used. Feel free and maybe code something better.
+//
+// Author: Jonas Kwiedor (aka gowizzard)
+//
+//********************************************************************************************************************//
+
+package gotimev2
