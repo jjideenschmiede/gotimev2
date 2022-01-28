2c882b8c4382a83fc70898b2fe45ab6ea0f9e953
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2022-01-28 15:42:24 +0100

    Add config function

--- config.go
+++ config.go
@@ -10,3 +10,53 @@
 //********************************************************************************************************************//
 
 package gotimev2
+
+import (
+	"bytes"
+	"fmt"
+	"net/http"
+)
+
+const (
+	protocol = "https"
+	host     = "timev2.de"
+)
+
+// Config is to define config data
+type Config struct {
+	Path, Method string
+	Body         []byte
+}
+
+// Request is to define the request data
+type Request struct {
+	SubDomain, Token string
+}
+
+// Send is to send a new request
+func (c *Config) Send(r Request) (*http.Response, error) {
+
+	// Set url
+	url := fmt.Sprintf("%s://%s.%s%s", protocol, r.SubDomain, host, c.Path)
+
+	// Define client
+	client := &http.Client{}
+
+	// Request
+	request, err := http.NewRequest(c.Method, url, bytes.NewBuffer(c.Body))
+	if err != nil {
+		return nil, err
+	}
+
+	request.Header.Set("authentication", r.Token)
+
+	// Send request & get response
+	response, err := client.Do(request)
+	if err != nil {
+		return nil, err
+	}
+
+	// Return data
+	return response, nil
+
+}
